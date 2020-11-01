21bee6bc8a64608af33b9e34b73428c025eee87a
chunga <clintonchunga@gmail.com>
2020-11-01 13:03:08 +0100

    adding a few currections to my code

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ $(document).ready(function () {
     // this should only occur on first time the app is loaded in the browser
     // helpfully remind user that lunch is important
     planTextArr = new Array(9);
-    planTextArr[4] = "Picnic lunch outside";
+    planTextArr[4] = "clinton is testing";
   }
 
   if (test) {
